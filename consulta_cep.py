591a4b0cf2a9175d84276d10d216af83d5d9adc9
VictorHOliveira <victor.oliver87@gmail.com>
2024-03-09 10:50:48 +0000

    Update consulta_cep.py

--- consulta_cep.py
+++ consulta_cep.py
@@ -2,6 +2,8 @@ import requests
 from tkinter import *
 from ttkthemes import *
 
+background_color = "#3c3c3c"
+
 def consultar_cep():
     obter_cep = cep.get()
     url = f'https://viacep.com.br/ws/{obter_cep}/json/'
@@ -42,35 +44,36 @@ def limpar_rotulos():
     lbl_estado.config(text="")
 
 janela = ThemedTk(theme="radiance")
-janela.title("Study Creation Interface!")
-janela.configure(bg="#3c3c3c")
+janela.title("Consulta CEP")
+janela.geometry("800x600")
+janela.configure(bg=background_color)
 
-text_title = Label(janela, text="Digite o CEP que deseja encontrar:", padx=10, pady=10)
-text_title.grid(column=0, row=0)
+text_title = Label(janela, text="Digite o CEP que deseja encontrar:", bg=background_color, fg="white", padx=5, pady=5)
+text_title.pack()
 
 cep = StringVar()
-entry_cep = Entry(janela, textvariable=cep)
-entry_cep.grid(column=0, row=1)
+entry_cep = Entry(janela, textvariable=cep, font=("Arial", 14))
+entry_cep.pack(pady=10)
 
-botao = Button(janela, text="Search", command=consultar_cep, bg="#4CAF50", fg="white")
-botao.grid(column=0, row=3)
+botao = Button(janela, text="Search", command=consultar_cep, bg="#8c8c8c", fg="white")
+botao.pack()
 
-lbl_cep = Label(janela, text="", bg="#3c3c3c", fg="white", padx=5, pady=5)
-lbl_cep.grid(column=0, row=4)
+lbl_cep = Label(janela, text="", bg=background_color, fg="white", padx=5, pady=5)
+lbl_cep.pack()
 
-lbl_logradouro = Label(janela, text="", bg="#3c3c3c", fg="white", padx=5, pady=5)
-lbl_logradouro.grid(column=0, row=5)
+lbl_logradouro = Label(janela, text="", bg=background_color, fg="white", padx=5, pady=5)
+lbl_logradouro.pack()
 
-lbl_complemento = Label(janela, text="", bg="#3c3c3c", fg="white", padx=5, pady=5)
-lbl_complemento.grid(column=0, row=6)
+lbl_complemento = Label(janela, text="", bg=background_color, fg="white", padx=5, pady=5)
+lbl_complemento.pack()
 
-lbl_bairro = Label(janela, text="", bg="#3c3c3c", fg="white", padx=5, pady=5)
-lbl_bairro.grid(column=0, row=7)
+lbl_bairro = Label(janela, text="", bg=background_color, fg="white", padx=5, pady=5)
+lbl_bairro.pack()
 
-lbl_cidade = Label(janela, text="", bg="#3c3c3c", fg="white", padx=5, pady=5)
-lbl_cidade.grid(column=0, row=8)
+lbl_cidade = Label(janela, text="", bg=background_color, fg="white", padx=5, pady=5)
+lbl_cidade.pack()
 
-lbl_estado = Label(janela, text="", bg="#3c3c3c", fg="white", padx=5, pady=5)
-lbl_estado.grid(column=0, row=9)
+lbl_estado = Label(janela, text="", bg=background_color, fg="white", padx=5, pady=5)
+lbl_estado.pack()
 
 janela.mainloop()
